0789db761da2736a63daaede932d2356ce3839fc
Kevin Leung <kevinresol@gmail.com>
2019-08-29 16:20:32 +0800

    Support firebase

---
+++ src/why/auth/FirebaseDelegate.hx
@@ -0,0 +1,48 @@
+package why.auth;
+
+import firebase.User;
+import firebase.auth.Auth;
+import tink.state.*;
+using tink.CoreApi;
+
+class FirebaseDelegate implements Delegate<Credentials, User> {
+	public var status(default, null):Observable<Status<User>>;
+	
+	var auth:Auth;
+	
+	public function new(auth) {
+		var state = new State<Status<User>>(Initializing);
+		auth.onAuthStateChanged(function(user) {
+			if(user != null) {
+				state.set(SignedIn(user));
+			} else {
+				state.set(SignedOut);
+			}
+		});
+		status = state.observe();
+	}
+	
+	public function signIn(credentials:Credentials):Promise<User> {
+		return switch credentials {
+			case Email(email, password):
+				Promise.ofJsPromise(auth.signInWithEmailAndPassword(email, password))
+					.next(cred -> cred.user);
+		}
+	}
+	
+	public function signOut():Promise<Noise> {
+		return Promise.ofJsPromise(auth.signOut());
+		
+	}
+	
+	public function getToken():Promise<Option<String>> {
+		return switch status.value {
+			case SignedIn(user): Promise.ofJsPromise(user.getIdToken()).next(Some);
+			case _: Promise.resolve(None);
+		}
+	}
+}
+
+enum Credentials {
+	Email(email:String, password:String);
+}
