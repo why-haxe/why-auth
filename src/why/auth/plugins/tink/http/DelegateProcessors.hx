a32edb93b003d8f5a72372134bce91708634c772
Kevin Leung <kevinresol@gmail.com>
2019-09-18 17:50:20 +0800

    Use the newer processor API

---
+++ src/why/auth/plugins/tink/http/DelegateProcessors.hx
@@ -0,0 +1,27 @@
+package why.auth.plugins.tink.http;
+
+#if !tink_http
+	#error 'Plugin tink.http.DelegateProcessors requires the tink_http library'
+#end
+
+import why.Delegate;
+import tink.http.Client;
+import tink.http.Request;
+import tink.http.Header;
+
+using tink.CoreApi;
+
+abstract DelegateProcessors(Processors) to Processors {
+	public function new<S, C, P, T>(delegate:Delegate<S, C, P, T>, scheme = 'Bearer') {
+		this = {
+			before: [req ->
+				switch delegate.status.value {
+					case SignedIn(user): user.getToken().next(token -> new OutgoingRequest(req.header.concat([new HeaderField(AUTHORIZATION, '$scheme $token')]), req.body));
+					case SignedOut: Promise.resolve(req);
+					case Initializing: Promise.reject(new Error('Delegate still initializing'));
+					case Errored(e): Promise.reject(e);
+				}
+			]
+		}
+	}
+}
