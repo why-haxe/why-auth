dcba88295b532473c459e7d44f370b606a1a8bb9
Kevin Leung <kevinresol@gmail.com>
2019-09-30 22:40:40 +0800

    Update dummy

--- src/why/auth/DummyDelegate.hx
+++ src/why/auth/DummyDelegate.hx
@@ -1,33 +1,39 @@
 package why.auth;
 
 import tink.state.*;
+import why.Delegate;
+
 using tink.CoreApi;
 
-class DummyDelegate<Credentials, Info> implements Delegate<Credentials, Info> {
-	public var status(default, null):Observable<Status<Info>>;
+class DummyDelegate<SignUpInfo, Credentials, Profile, ProfilePatch> implements Delegate<SignUpInfo, Credentials, Profile, ProfilePatch> {
+	public var status(default, null):Observable<Status<User<Profile, ProfilePatch>>>;
 	
-	var state:State<Status<Info>>;
+	var state:State<Status<User<Profile, ProfilePatch>>>;
 	var credentials:Credentials;
-	var getInfo:Credentials->Info;
+	var getInfo:Credentials->Profile;
 	var _getToken:Credentials->Promise<String>;
 	
 	public function new(getInfo, getToken, ?init) {
 		this.credentials = init;
 		this.getInfo = getInfo;
 		this._getToken = getToken;
-		state = new State<Status<Info>>(Initializing);
+		state = new State<Status<User<Profile, ProfilePatch>>>(Initializing);
 		status = state.observe();
 		update();
 	}
 	
 	function update() {
-		state.set(credentials == null ? SignedOut : SignedIn(getInfo(credentials)));
+		state.set(credentials == null ? SignedOut : SignedIn(new DummyUser(function() return _getToken(credentials), getInfo(credentials))));
 	}
 	
-	public function signIn(credentials:Credentials):Promise<Info> {
+	public function signUp(info:SignUpInfo):Promise<Noise> {
+		return new Error(NotImplemented, 'DummyDelegate#signUp is not implemented');
+	}
+	
+	public function signIn(credentials:Credentials):Promise<Noise> {
 		this.credentials = credentials;
 		update();
-		return getInfo(credentials);
+		return Promise.NOISE;
 	}
 	
 	public function signOut():Promise<Noise> {
@@ -39,4 +45,39 @@ class DummyDelegate<Credentials, Info> implements Delegate<Credentials, Info> {
 	public function getToken():Promise<Option<String>> {
 		return credentials == null ? None : _getToken(credentials).next(Some);
 	}
+	
+	public function forgetPassword(id:String):Promise<Noise> {
+		return new Error(NotImplemented, 'DummyDelegate#forgetPassword is not implemented');
+	}
+	
+	public function resetPassword(id:String, code:String, password:String):Promise<Noise> {
+		return new Error(NotImplemented, 'DummyDelegate#resetPassword is not implemented');
+	}
+	
+	public function confirmSignUp(id:String, code:String):Promise<Noise> {
+		return new Error(NotImplemented, 'DummyDelegate#confirmSignUp is not implemented');
+	}
+}
+
+class DummyUser<Profile, ProfilePatch> implements User<Profile, ProfilePatch> {
+	public var profile(default, null):Observable<Profile>;
+	var _getToken:Void->Promise<String>;
+	
+	public function new(getToken, initProfile) {
+		profile = new State(initProfile);
+		_getToken = getToken;
+	}
+	
+	public function getToken():Promise<String> {
+		return _getToken();
+	}
+	
+	public function updateProfile(patch:ProfilePatch):Promise<Noise> {
+		return new Error(NotImplemented, 'DummyUser#confirmSignUp is not implemented');
+	}
+	
+	public function changePassword(oldPassword:String, newPassword:String):Promise<Noise> {
+		return new Error(NotImplemented, 'DummyUser#confirmSignUp is not implemented');
+	}
+	
 }
