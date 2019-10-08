851b8135049d28368d200a2a7048806cd12ff5a5
Kevin Leung <kevinresol@gmail.com>
2019-10-08 16:26:54 +0800

    Support Auth0

---
+++ src/why/auth/Auth0Auth.hx
@@ -0,0 +1,48 @@
+package why.auth;
+
+import jsonwebtoken.Claims;
+import jsonwebtoken.verifier.BasicVerifier;
+import why.auth.JwkAuth;
+
+using tink.CoreApi;
+
+class Auth0Auth<User> extends JwkAuth<Auth0Profile, User>{
+	
+	public function new(config:Auth0Config<User>) {
+		super({
+			makeUser: config.makeUser,
+			jwkUrl: 'https://${config.domain}/.well-known/jwks.json',
+			token: config.token,
+			options: {
+				iss: 'https://${config.domain}/',
+				aud: config.clientId,
+			},
+		});
+	}
+	
+	public static inline function verifyToken<User>(input:VerifyInput):Promise<Claims> {
+		return JwkAuth.verifyToken({
+			jwkUrl: 'https://${input.domain}/.well-known/jwks.json',
+			token: input.token,
+			options: {
+				iss: 'https://${input.domain}/',
+				aud: input.clientId,
+			},
+		});
+	}
+}
+
+typedef Auth0Profile = {
+	> Claims,
+}
+
+typedef Auth0Config<User> = {
+	> VerifyInput,
+	var makeUser(default, null):Auth0Profile->Promise<Option<User>>;
+}
+
+private typedef VerifyInput = {
+	var domain(default, null):String;
+	var clientId(default, null):String;
+	var token(default, null):String;
+}
