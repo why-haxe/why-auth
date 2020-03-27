426f3e52e7e4037fe2919782cdd787a8fd7adc07
Kevin Leung <kevinresol@gmail.com>
2020-03-27 20:39:01 +0800

    Add postprocess tool

--- src/why/auth/Status.hx
+++ src/why/auth/Status.hx
@@ -35,3 +35,34 @@ class StatusTools {
 		}
 	}
 }
+
+#if tink_state
+class ObservableStatusTools {
+	public static function postprocess<P, R>(observable:tink.state.Observable<Status<P>>, f:P->Promise<R>):tink.state.Observable<Status<R>> {
+		var last = switch observable.value {
+			case Initializing: Initializing;
+			case Errored(e):  Errored(e);
+			case _: SignedOut;
+		}
+		return observable
+			.mapAsync(function(status) return switch status {
+				case Initializing:
+					Future.sync(last = Initializing);
+				case SignedOut:
+					Future.sync(last = SignedOut);
+				case SignedIn(profile):
+					f(profile).map(function(o) return switch o {
+						case Success(v): SignedIn(v);
+						case Failure(e): Errored(e);
+					});
+				case Errored(e):
+					Future.sync(last = Errored(e));
+			})
+			.map(promised -> switch promised {
+				case Loading: last;
+				case Done(v): v;
+				case Failed(e): why.auth.Status.Errored(e);
+			});
+	}
+}
+#end
